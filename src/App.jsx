319fdf1f5c86c1c667614a7128f3c0666aa22816
LUCKY RANJAN <ranjan.lucky543@gmail.com>
2023-08-18 18:08:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+
+import StartGame from "./components/StartGame"
+import './App.css'
+import GamePlay from './components/GamePlay';
+
+const App = () => {
+
+  const [isGameStarted, setIsGameStarted] = useState(false)
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev)
+  }
+
+  return (
+    <>
+      {
+        isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />
+      }
+    </>
+  )
+}
+
+export default App
